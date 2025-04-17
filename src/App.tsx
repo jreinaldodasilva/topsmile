9b1dc8332325802b0bcb261b4f4b9df7fe8b2fc7
jreinaldodasilva <jreinaldo.dasilva@outlook.com>
2025-04-17 11:14:42 -0300

    Updating Landing Page Adding Home Page and Updating App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Features from './components/Features/Features';
-import Specialties from './components/Specialties/Specialties';
-import Testimonials from './components/Testimonials/Testimonials';
-import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 import './styles/global.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <Header />
-      <Hero />
-      <Features />
-      <Specialties />
-      <Testimonials />
-      <Footer />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* futuras rotas: /login, /cadastro, etc. */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
