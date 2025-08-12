e80056e023578a8a8e3f570bec34d6baaeeab581
jreinaldodasiva <jreinaldo.dasilva@outlook.com>
2025-08-12 09:49:25 -0300

    Adding ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ErrorBoundary from './components/ErrorBoundary';
 import Home from './pages/Home/Home';
 import './styles/global.css';
 
 const App: React.FC = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
+  <ErrorBoundary>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  </ErrorBoundary>
+
 );
 
 export default App;
